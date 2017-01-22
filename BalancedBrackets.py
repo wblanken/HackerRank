06be114e971883387f5a79b0dc650aa28da8ef99
wblanken <blankenship.will@gmail.com>
2017-01-21 21:34:00 -0600

    Stacks for the win!

--- BalancedBrackets.py
+++ BalancedBrackets.py
@@ -17,44 +17,35 @@ By this logic, we say a sequence of brackets is considered to be balanced if the
 
 Given n strings of brackets, determine whether each sequence of brackets is balanced. If a string is balanced, print YES on a new line; otherwise, print NO on a new line.
 """
-from collections import deque
-
-
-def check_brackets(brackets):
-    is_balanced = False
-    front = brackets.popleft()
-    back = brackets.pop()
-    
-    if front == '{' and back == '}':
-        is_balanced = True
-    elif front == '[' and back == ']':
-        is_balanced = True
-    elif front == '(' and back == ')':
-        is_balanced = True
-
-    if is_balanced == True and len(brackets) > 0:
-        is_balanced = check_brackets(brackets)
-
-    return is_balanced
 
 
 def is_matched(expression):
     if len(expression) % 2 == 0:
-        brackets = deque(expression)
-        return check_brackets(brackets)        
-    return False
+        brackets = []
+        for c in expression:
+            if c == '{':
+                brackets.append('}')
+            elif c == '[':
+                brackets.append(']')
+            elif c == '(':
+                brackets.append(')')
+            else:
+                if len(brackets) == 0 or c != brackets[-1]:
+                    return False
+                brackets.pop()
+    return len(brackets) == 0
     
 
 # Test input and find failures
-infile = open("test cases/Bracket_Test_In.txt", 'r')
-outfile = open("test cases/Bracket_Test_Out.txt", 'r')
+infile = open("test cases/Bracket_Test_In.1.txt", 'r')
+outfile = open("test cases/Bracket_Test_Out.1.txt", 'r')
 
 t = infile.readline()
 inputs = infile.readlines()
 answers = outfile.readlines()
 for a0 in range(int(t.strip())):
     expression = inputs[a0].strip()
-    expected_result = True if answers[a0] == "YES" else False
+    expected_result = True if answers[a0].replace('\n','') == "YES" else False
     if is_matched(expression) == expected_result:
         print(str(a0) + " PASS")
     else:
